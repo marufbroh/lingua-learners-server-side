26f4333747ada3e4d2156a09d8c242072afe8fc8
marufbroh <marufmd775@gmail.com>
2023-06-08 10:53:11 +0600

    folder name changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+//middleware // I changed my folder name, Im checking it it working or not
 app.use(cors());
 app.use(express.json());
 
